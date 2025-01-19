006077d6ea3a572e068487a1e24374088f135e25
CodeNinjaDan <dnlmuiru@gmail.com>
2025-01-19 13:22:50 +0300

    scrape & track products

--- amazon-price-tracker/main.py
+++ amazon-price-tracker/main.py
@@ -11,7 +11,7 @@ recipient_email = os.getenv("RECIPIENT_EMAIL")
 email_password = os.getenv("EMAIL_PASSWORD")
 smtp_server = os.getenv("SMTP_SERVER")
 
-URL = "https://appbrewery.github.io/instant_pot/"
+URL = "https://www.amazon.com/dp/B0CN8HL597/ref=sspa_dk_detail_6?s=kitchen&sp_csd=d2lkZ2V0TmFtZT1zcF9kZXRhaWw"
 header = {"Accept-Language": "en-US,en",
 "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
 }
@@ -19,12 +19,16 @@ response = requests.get(url=URL, headers=header)
 amazon_page = response.text
 
 soup = BeautifulSoup(amazon_page, 'html.parser')
-whole_price = soup.find(name="span", class_="a-price-whole").getText()
-decimal_price = soup.find(name="span", class_="a-price-fraction").getText()
+print(soup.prettify())
+whole_price = soup.find(name="span", class_="a-size-base a-color-base").getText()
+decimal_price = soup.find(name="span", class_="a-size-base a-color-base").getText()
+
+#Fix this so that the total price is fetched(price+shipping+import tax)
+
 price = float(whole_price) + float(decimal_price)
 product_name = soup.select("span.a-size-large.product-title-word-break")[0].getText()
 
-if price < 100:
+if price < 1000:
     subject = "Amazon Price Alert!"
     body = f"{product_name} is now ${price}"
     msg=f"Subject:{subject}\n\n{body}".encode('utf-8')
